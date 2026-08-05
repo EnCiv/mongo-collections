9068f596eb814f6985a1d7a69891dee74dfebb5d
David Fridley <ddfridley@yahoo.com>
2026-08-04 18:09:38 -0700

    fixes for git action

--- jest-db.config.js
+++ jest-db.config.js
@@ -1,5 +1,4 @@
 module.exports = {
     testTimeout: 5000000,
-    setupFilesAfterEnv: ['<rootDir>/node_modules/jest-enzyme/lib/index.js'],
     preset: '@shelf/jest-mongodb',
 }
--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-    setupFilesAfterEnv: ['<rootDir>/node_modules/jest-enzyme/lib/index.js'],
     preset: '@shelf/jest-mongodb',
     watchPathIgnorePatterns: ['<rootDir>/tmp/', '<rootDir>/node_modules/'],
     roots: ['<rootDir>'],
